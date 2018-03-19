b10105a2432d50af7c891339d3cee51a5a398d0a
Zhihao Cao <josephzelo@hotmail.com>
2018-03-18 22:09:14 -0400

    bigtable component

--- Service/main.go
+++ Service/main.go
@@ -8,7 +8,10 @@ import (
 	"strconv"
 	"gopkg.in/olivere/elastic.v3"
 	"reflect"
-	 "github.com/pborman/uuid"
+	"context"
+	"cloud.google.com/go/bigtable"
+	"github.com/pborman/uuid"
+	"strings"
 )
 
 const (
@@ -16,8 +19,8 @@ const (
 	TYPE = "post"
 	DISTANCE = "200km"
 	// Needs to update
-	//PROJECT_ID = "around-xxx"
-	//BT_INSTANCE = "around-post"
+	PROJECT_ID = "aurora-198420"
+	BT_INSTANCE = "aurora-post"
 	// Needs to update this URL if you deploy it to cloud.
 	ES_URL = "http://104.198.25.122:9200"
 )
@@ -113,6 +116,30 @@ func saveToES(p *Post, id string) {
 	}
 
 	fmt.Printf("Post is saved to Index: %s\n", p.Message)
+
+	ctx := context.Background()
+	// you must update project name here
+	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	tbl := bt_client.Open("post")
+	mut := bigtable.NewMutation()
+	t := bigtable.Now()
+
+	mut.Set("post", "user", t, []byte(p.User))
+	mut.Set("post", "message", t, []byte(p.Message))
+	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+	err = tbl.Apply(ctx, id, mut)
+	if err != nil {
+		panic(err)
+		return
+	}
+	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
 }
 
 func handlerSearch(w http.ResponseWriter, r *http.Request) {
@@ -166,7 +193,12 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 		p := item.(Post) // p = (Post) item
 		fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
 		// TODO(student homework): Perform filtering based on keywords such as web spam etc.
-		ps = append(ps, p)
+		if !containsFilteredWords(&p.Message) {
+			ps = append(ps, p)
+		} else {
+			fmt.Printf("This post contains inappropriate words\n");
+		}
+
 
 	}
 	js, err := json.Marshal(ps)
@@ -180,3 +212,15 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
 	w.Write(js)
 }
 
+func containsFilteredWords(s *string) bool {
+	filteredWords := []string{
+		"fuck",
+		"ass",
+	}
+	for _, word := range filteredWords {
+		if strings.Contains(*s, word) {
+			return true
+		}
+	}
+	return false
+}
